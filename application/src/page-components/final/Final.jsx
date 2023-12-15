2fed15ac4a3b1b81735d3220d27eb502335f9157
Henna Kotilainen <henna.kotilainen@vuonogroup.com>
2023-12-15 15:16:44 +0200

    add back button and progress indicator to all pages, change text color

--- application/src/page-components/final/Final.jsx
+++ application/src/page-components/final/Final.jsx
@@ -1,7 +1,10 @@
-
+import { AlignSelf } from "../../components/divs";
+import { ProgressIndicator } from "../../components/ProgressIndicator";
 const Final = () => {
     return (
         <>
+        <AlignSelf> <a href="/appointment">{"< Back"}</a></AlignSelf> 
+        <ProgressIndicator currentStep={5}/>
         <p>Final page, thanx for booking the appointment etc. :D</p>
         </>
         
